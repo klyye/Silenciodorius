322b586b2c0836f0b55393245b9dfbb9a8bbb256
Kyle Zhang <zkyle1337@gmail.com>
2020-06-09 01:09:28 -0700

    Github unity is buggy and it takes two commits to get all my changes across for some reason

---
+++ Assets/Scripts/Direction.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// The four cardinal directions.
+/// </summary>
+[Serializable]
+public enum Direction
+{
+    North, East, South, West
+}
