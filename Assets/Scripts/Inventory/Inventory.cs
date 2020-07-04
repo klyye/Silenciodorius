5fe647514153b48cb9b8757cd618526137492c4e
Kyle Zhang <zkyle1337@gmail.com>
2020-07-04 00:32:57 -0700

    Started items/inventory a little

--- Assets/Scripts/Inventory/Inventory.cs
+++ Assets/Scripts/Inventory/Inventory.cs
@@ -1,7 +1,12 @@
-﻿namespace Inventory
+﻿using System.Collections.Generic;
+
+namespace Inventory
 {
     public class Inventory
     {
-        
+        /// <summary>
+        ///     Maps items in the inventory to the amount of items the unit holds.
+        /// </summary>
+        private IDictionary<Item, int> _items = new Dictionary<Item, int>();
     }
 }
--- Assets/Scripts/Inventory/Item.cs
+++ Assets/Scripts/Inventory/Item.cs
@@ -4,6 +4,24 @@ namespace Inventory
 {
     public class Item : MonoBehaviour
     {
-        
+        /// <summary>
+        ///     What this item is called. Each type of Item has a unique name.
+        /// </summary>
+        private string _name;
+
+        public override int GetHashCode()
+        {
+            return _name.GetHashCode();
+        }
+
+        public override bool Equals(object other)
+        {
+            return other != null && other is Item && ((Item)other)._name.Equals(_name);
+        }
+
+        public override string ToString()
+        {
+            return _name;
+        }
     }
 }
