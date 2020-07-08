27338302775413f0ec0b26f43aae1ef1b07422be
Kyle Zhang <zkyle1337@gmail.com>
2020-07-08 13:43:52 -0700

    Updated Unity project version

---
+++ Assets/Scripts/Items/Weapons/BroomHandle.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace Items.Weapons
+{
+    /// <summary>
+    ///     A basic melee weapon.
+    /// </summary>
+    public class BroomHandle : Weapon
+    {
+        public override void Attack(Vector2 target, bool isEnemy)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
--- Assets/Scripts/LevelGeneration/Chunk.cs
+++ Assets/Scripts/LevelGeneration/Chunk.cs
@@ -6,7 +6,6 @@ namespace LevelGeneration
     /// <summary>
     ///     Represents a LENGTH x LENGTH section of lines. This is the basic unit used in MapGenerator.cs.
     /// </summary>
-    [RequireComponent(typeof(Rigidbody2D))]
     [RequireComponent(typeof(Collider2D))]
     public abstract class Chunk : MonoBehaviour
     {
