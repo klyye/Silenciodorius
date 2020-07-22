bf5d1cf91d814bd9f9c124d581a5843e7df9050a
Kyle Zhang <zkyle1337@gmail.com>
2020-07-22 00:30:13 -0700

    basic ui setup

---
+++ Assets/Scripts/UI/UIManager.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+namespace UI
+{
+    /// <summary>
+    ///     Handles the user interface.
+    /// </summary>
+    [RequireComponent(typeof(Canvas))]
+    public class UIManager : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+    }
+}
