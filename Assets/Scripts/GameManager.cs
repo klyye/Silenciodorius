fe0ce1645bbc07816d9d1c37b0b2ad485a4ff949
Kyle Zhang <zkyle1337@gmail.com>
2020-08-30 21:09:13 -0700

    Updated unity version

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -44,6 +44,7 @@ public class GameManager : MonoBehaviour
         player = FindObjectOfType<Player>();
         player.OnPlayerDeath += GameOver;
         uiManager = FindObjectOfType<UIManager>();
+        QualitySettings.vSyncCount = 1;    //Really bad screen tearing
         Resume();
     }
 
